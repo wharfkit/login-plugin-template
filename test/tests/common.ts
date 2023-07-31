0610c4b3c928106b946d61af2776f69d97a0aff5
Aaron Cox <aaron@greymass.com>
2023-07-31 14:53:15 -0700

    Updating for 1.0.0 release

--- test/tests/common.ts
+++ test/tests/common.ts
@@ -1,31 +1,15 @@
-import {mockFetch} from '$test/utils/mock-fetch'
-import {MockUserInterface} from '$test/utils/mock-ui'
-
 import {LoginPluginTemplate} from '../../src/index'
 
-import {SessionKit, SessionKitOptions} from '@wharfkit/session'
-import {WalletPluginPrivateKey} from '@wharfkit/wallet-plugin-privatekey'
-
-const wallet = new WalletPluginPrivateKey('5Jtoxgny5tT7NiNFp1MLogviuPJ9NniWjnU4wKzaX4t7pL4kJ8s')
-
-const mockSessionKitOptions: SessionKitOptions = {
-    appName: 'myapp',
-    chains: [
-        {
-            id: '73e4385a2708e6d7048834fbc1079f2fabb17b3c125b146af438971e90716c4d',
-            url: 'http://jungle4.greymass.com',
-        },
-    ],
-    fetch: mockFetch,
-    loginPlugins: [new LoginPluginTemplate()],
-    ui: new MockUserInterface(),
-    walletPlugins: [wallet],
-}
+import {SessionKit} from '@wharfkit/session'
+import {mockSessionKitArgs, mockSessionKitOptions} from '@wharfkit/mock-data'
 
 suite('example', function () {
-    test('plugin usage', async function () {
-        const kit = new SessionKit(mockSessionKitOptions)
-        const result = await kit.login()
-        console.log(result)
-    })
+    // test('plugin usage', async function () {
+    //     const kit = new SessionKit(mockSessionKitArgs, {
+    //         ...mockSessionKitOptions,
+    //         loginPlugins: [new LoginPluginTemplate()],
+    //     })
+    //     const result = await kit.login()
+    //     console.log(result)
+    // })
 })
